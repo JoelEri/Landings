2f7f3b16cdfd38dd87b45dc42b4343f062baf675
JoelEri <tismma20joeeri@innovationsgymnasiet.se>
2021-09-28 10:31:02 +0200

    "include" kod inne

--- db_connnection.php
+++ db_connnection.php
@@ -1,10 +1,11 @@
 <?php
+include connection.php
 function OpenCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "1234";
- $db = "example";
+ $dbpass = "";
+ $db = "test";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
  return $conn;
