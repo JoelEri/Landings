ca5b60e4c628dd75c3cd3d683c49fa23d0736f7e
JoelEri <tismma20joeeri@innovationsgymnasiet.se>
2021-10-11 13:33:26 +0200

    sparade

--- webbserv1-main/db_connection.php
+++ webbserv1-main/db_connection.php
@@ -19,7 +19,7 @@ $query = mysqli_query($conn, "SELECT * FROM tbl_articles");
 while($row = mysqli_fetch_assoc($query))
 {
 $title = $row[´title´];
-$content = $row [´content´]
+$content = $row [´content´];
 }
 
 
